02632f39036f7da10e7aaa68433a65bc51857c17
Thomas Carpenter <108958853+tgcarpenter@users.noreply.github.com>
2022-10-18 18:59:06 -0700

    Update Databases.py

--- Databases.py
+++ Databases.py
@@ -7,13 +7,13 @@ class Database:
         self.text_table_count = 1  # defines current name for text tables in column creation
         self.list_table_count = 1  # defines current name for list tables in column creation
         self.row_count = 0  # defines current row count
-        self.class_count = 0  # defines position of created column classes in column_classes list
+        self.class_count = 0  # defines position of created column classes in column_classes list  -- useless?
         # what if two columns are named the same thing? ugh!! <- fixed because I refer to them by position not name
         self.column_classes = []  # contains all the column class objects (in order of columns)
         con = sql.connect(self.name)
         cur = con.cursor()
         try:
-            cur.execute('CREATE TABLE Main (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)')
+            cur.execute('CREATE TABLE Main (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)')
         except:
             pass
         con.commit()
@@ -58,15 +58,18 @@ class Database:
         self.column_classes.remove(self.column_classes[c_id])
 
     def delete_row(self, old_values: list, r_id):
-        self.row_count -= 1
-        for column, old_val in zip(self.column_classes, old_values):
-            if column.table_name[0] == "T":
-                column.delete_row(self.name, old_val)
-            elif column.table_name[0] == "L":
-                column.delete_row(self.name, old_val, None, r_id)
         con = sql.connect(self.name)
         cur = con.cursor()
-        cur.execute(f'DELETE FROM Main WHERE _rowid_ = {r_id}')
+        self.row_count -= 1
+        for column, old_val in zip(self.column_classes, old_values):
+            if column.table_name[1] == "T":
+                column.delete_row(self.name, old_val)
+            elif column.table_name[1] == "L":
+                column.delete_row(self.name, old_val, None, r_id)
+                cur.execute(f'UPDATE {column.link_table_name} SET _main_id = _main_id - 1 WHERE _main_id > {r_id}')
+        cur.execute(f'DELETE FROM Main WHERE _id = {r_id}')
+        cur.execute(f'UPDATE Main SET _id = _id - 1 WHERE _id > {r_id}')
+        cur.execute(f'DELETE FROM "sqlite_sequence" WHERE "name" = "Main"')
         con.commit()
         con.close()
 
@@ -86,18 +89,33 @@ class Database:
         cur = con.cursor()
         cur.execute("INSERT INTO Main DEFAULT VALUES")
         for column in self.column_classes:
-            if column.table_name[0] == 'L':
-                cur.execute(f'INSERT INTO {column.link_table_name} (main_id) VALUES ((SELECT MAX(id) FROM Main))')
+            if column.table_name[1] == 'L':
+                cur.execute(f'INSERT INTO {column.link_table_name} (_main_id) VALUES ((SELECT MAX(_id) FROM Main))')
         con.commit()
         con.close()
 
-    def get_column_headers(self):
-        return [column.column_name[1:-1] for column in self.column_classes]
+    def get_column_headers(self) -> list:
+        return ["id"] + [column.column_name[2:-1] for column in self.column_classes]
 
-    def general_query(self, start, end):
-        select_table_columns = ''
+    def autofill_query(self, search):
+        case = "CASE "
+        col = "FROM "
+        con = sql.connect(self.name)
+        cur = con.cursor()
+        for column in self.column_classes:
+            col += f"{column.table_name} JOIN "
+            case += f"WHEN {column.table_name}.{column.column_name} LIKE '{search}%' " \
+                    f"THEN TRIM({column.table_name}.{column.column_name}, ' %') "
+        autofill = cur.execute(f'SELECT DISTINCT {case} END C {col[:-6]} '
+                               f'WHERE C IS NOT NULL AND C != "{search}" ORDER BY C').fetchall()
+        print("auto", autofill)
+        return autofill
+
+    def general_query(self, start=int, end=int, search=False):  # search holds the text to search for. start/end will
+        select_table_columns = ''                               # not be given if search is given
         joined_tables = ''
         on_parameters = ''
+        where = 'WHERE ('
         count = 1
         switch = True
         con = sql.connect(self.name)
@@ -108,36 +126,42 @@ class Database:
                 select_table_columns += ' '
             else:
                 select_table_columns += ', '
-            if column.table_name[0] == 'T':  # Generating join tables and on parameters
+            if column.table_name[1] == 'T':  # Generating join tables and on parameters
                 joined_tables += 'JOIN ' + column.table_name + ' '
                 if switch:
                     switch = False
-                    on_parameters += f'ON Main.{column.table_name.casefold()}_id = {column.table_name}.id '
+                    on_parameters += f'ON Main.{column.table_name.casefold()}_id = {column.table_name}._rowid_ '
                 else:
-                    on_parameters += f'AND Main.{column.table_name.casefold()}_id = {column.table_name}.id '
-            elif column.table_name[0] == 'L':
-                joined_tables += f'JOIN (SELECT DISTINCT {column.link_table_name}.main_id, ' \
+                    on_parameters += f'AND Main.{column.table_name.casefold()}_id = {column.table_name}._rowid_ '
+            elif column.table_name[1] == 'L':
+                joined_tables += f'JOIN (SELECT DISTINCT {column.link_table_name}._main_id, ' \
                                  f'group_concat({column.table_name}.{column.column_name}, ", ") OVER (PARTITION BY ' \
-                                 f'Main.id) AS {column.column_name} FROM Main JOIN {column.table_name} JOIN ' \
-                                 f'{column.link_table_name} ON Main.id = {column.link_table_name}.main_id AND ' \
-                                 f'{column.table_name}.id = ' \
+                                 f'Main._id) AS {column.column_name} FROM Main JOIN {column.table_name} JOIN ' \
+                                 f'{column.link_table_name} ON Main._id = {column.link_table_name}._main_id AND ' \
+                                 f'{column.table_name}._rowid_ = ' \
                                  f'{column.link_table_name}.{column.table_name.casefold()}_id) AS {column.table_name}' \
                                  f' '
                 if switch:
                     switch = False
-                    on_parameters += f'ON Main.id = {column.table_name}.main_id '
+                    on_parameters += f'ON Main._id = {column.table_name}._main_id '
                 else:
-                    on_parameters += f'AND Main.id = {column.table_name}.main_id '
+                    on_parameters += f'AND Main._id = {column.table_name}._main_id '
+            if search:
+                where += fr'{column.column_name} LIKE "%{search}%" ESCAPE "\" OR '
             count += 1
-        query = cur.execute(f'SELECT {select_table_columns} FROM Main {joined_tables} {on_parameters} '
-                            f'WHERE Main._rowid_ >= {start} AND Main._rowid_ <= {end}').fetchall()
+        if search:
+            where = where[:-3] + ')'
+        else:
+            where = f'WHERE Main._id >= {start} AND Main._id <= {end}'
+        query = cur.execute(fr'SELECT Main._id, {select_table_columns} FROM Main {joined_tables} {on_parameters} '
+                            fr'{where}').fetchall()
         con.close()
         return query
 
 
 class ColumnInteger:
     def __init__(self, name, db_name):  # Inserts column into main table One-to-one
-        self.column_name = "[" + name + "]"
+        self.column_name = "[_" + name + "]"
         self.table_name = "Main"  # this is interesting
         con = sql.connect(db_name)
         cur = con.cursor()
@@ -174,21 +198,22 @@ class ColumnInteger:
         cur = con.cursor()
         if not new_value:
             cur.execute(f'UPDATE Main SET {self.column_name} = Null '
-                        f'WHERE _rowid_ = {r_id}')
+                        f'WHERE _id = {r_id}')
         else:
-            cur.execute(f'UPDATE Main SET {self.column_name} = "{new_value}" WHERE _rowid_ = {r_id}')
+            if new_value[0] == "_":
+                new_value = new_value[1:]
+            cur.execute(f'UPDATE Main SET {self.column_name} = "{new_value}" WHERE _id = {r_id}')
         con.commit()
         con.close()
 
 
 class ColumnText:
     def __init__(self, name, db_name, table_count):  # Creates a one-to-many relation between main table and text table
-        self.column_name = "[" + name + "]"
-        self.table_name = 'TextTable' + str(table_count)
+        self.column_name = "[_" + name + "]"
+        self.table_name = '_TextTable' + str(table_count)
         con = sql.connect(db_name)
         cur = con.cursor()
-        cur.execute(f'CREATE TABLE {self.table_name} (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, '
-                    f'{self.column_name} TEXT UNIQUE)')
+        cur.execute(f'CREATE TABLE {self.table_name} ({self.column_name} TEXT UNIQUE)')
         cur.execute(f'INSERT INTO {self.table_name} DEFAULT VALUES')
         cur.execute(f'ALTER TABLE Main ADD {self.table_name.casefold() + "_id"} INTEGER NOT NULL DEFAULT 1')
         con.commit()
@@ -207,15 +232,15 @@ class ColumnText:
         cur = con.cursor()
         if old_value:
             temp_old_value = '"' + old_value + '"'
-            temp = cur.execute(f'SELECT Main.id FROM Main JOIN {self.table_name} '
-                               f'WHERE Main.{self.table_name.casefold() + "_id"} = {self.table_name}.id '
+            temp = cur.execute(f'SELECT Main._id FROM Main JOIN {self.table_name} '
+                               f'WHERE Main.{self.table_name.casefold() + "_id"} = {self.table_name}._rowid_ '
                                f'AND {self.table_name}.{self.column_name} = {temp_old_value}').fetchall()
             if len(temp) == 1:
                 cur.execute(f'DELETE FROM {self.table_name} WHERE {self.column_name} = {temp_old_value}')
         con.commit()
         con.close()
 
-    def insert_into(self, db_name, *args):
+    def insert_into(self, db_name, *args):  # pretty sure this is useless
         values = ''
         count = 0
         for item in args:
@@ -231,7 +256,7 @@ class ColumnText:
             cur.execute(f'INSERT INTO {self.table_name} ({self.column_name}) VALUES (?)', (values,))
         except:
             pass
-        cur.execute(f'INSERT INTO Main ({self.table_name.casefold() + "_id"}) SELECT id FROM {self.table_name} '
+        cur.execute(f'INSERT INTO Main ({self.table_name.casefold() + "_id"}) SELECT _id FROM {self.table_name} '
                     f'WHERE {self.column_name} = "{values}"')
         con.commit()
         con.close()
@@ -243,11 +268,11 @@ class ColumnText:
             temp_old_value = '"' + old_value + '"'
         else:
             temp_old_value = 'NULL'
-        temp = cur.execute(f'SELECT Main.id FROM Main JOIN {self.table_name} '
-                           f'WHERE Main.{self.table_name.casefold() + "_id"} = {self.table_name}.id '
+        temp = cur.execute(f'SELECT Main._id FROM Main JOIN {self.table_name} '
+                           f'WHERE Main.{self.table_name.casefold() + "_id"} = {self.table_name}._rowid_ '
                            f'AND {self.table_name}.{self.column_name} = {temp_old_value}').fetchall()
         if not new_value:
-            cur.execute(f'UPDATE Main SET {self.table_name.casefold()}_id = 1 WHERE _rowid_ = {r_id}')
+            cur.execute(f'UPDATE Main SET {self.table_name.casefold()}_id = 1 WHERE _id = {r_id}')
             if len(temp) == 1 and temp_old_value != "NULL":
                 cur.execute(f'DELETE FROM {self.table_name} WHERE {self.column_name} = {temp_old_value}')
         elif len(temp) != 1:
@@ -255,9 +280,9 @@ class ColumnText:
                 cur.execute(f'INSERT INTO {self.table_name} ({self.column_name}) VALUES (?)', (new_value,))
             except:
                 pass
-            cur.execute(f'UPDATE Main SET {self.table_name.casefold()}_id = (SELECT {self.table_name}.id '
+            cur.execute(f'UPDATE Main SET {self.table_name.casefold()}_id = (SELECT _rowid_ '
                         f'FROM {self.table_name} WHERE {self.table_name}.{self.column_name} = "{new_value}") '
-                        f'WHERE _rowid_ = {r_id}')
+                        f'WHERE _id = {r_id}')
         else:
             cur.execute(f'UPDATE {self.table_name} SET {self.column_name} = (?) '
                         f'WHERE {self.column_name} = "{old_value}"', (new_value,))
@@ -267,18 +292,18 @@ class ColumnText:
 
 class ColumnList:
     def __init__(self, name, db_name, table_count):  # Creates a many-to-many relation between main table and list table
-        self.column_name = "[" + name + "]"
-        self.table_name = 'ListTable' + str(table_count)
-        self.link_table_name = 'LinkTable' + str(table_count)
+        self.column_name = "[_" + name + "]"
+        self.table_name = '_ListTable' + str(table_count)
+        self.link_table_name = '_LinkTable' + str(table_count)
         con = sql.connect(db_name)
         cur = con.cursor()
         cur.execute(f'CREATE TABLE {self.table_name} '  # create list table
-                    f'(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, {self.column_name} TEXT UNIQUE)')
+                    f'({self.column_name} TEXT UNIQUE)')
         cur.execute(f'INSERT INTO {self.table_name} DEFAULT VALUES')
         cur.execute(
             f'CREATE TABLE {self.link_table_name} ({self.table_name.casefold() + "_id"} INTEGER NOT NULL '  # Create link table
-            f'DEFAULT 1, main_id INTEGER NOT NULL, PRIMARY KEY ({self.table_name.casefold() + "_id"}, main_id))')
-        cur.execute(f'INSERT INTO {self.link_table_name} (main_id) SELECT Main.id FROM Main')
+            f'DEFAULT 1, _main_id INTEGER NOT NULL, PRIMARY KEY ({self.table_name.casefold() + "_id"}, _main_id))')
+        cur.execute(f'INSERT INTO {self.link_table_name} (_main_id) SELECT Main._id FROM Main')
         con.commit()
         con.close()
 
@@ -293,7 +318,7 @@ class ColumnList:
     def add_default_to_link_table(self, db_name, row_number): # not sure what this is for either
         con = sql.connect(db_name)
         cur = con.cursor()
-        cur.execute(f'INSERT INTO {self.link_table_name} (main_id) VALUES ({row_number})')
+        cur.execute(f'INSERT INTO {self.link_table_name} (_main_id) VALUES ({row_number})')
         con.commit()
         con.close()
 
@@ -306,10 +331,10 @@ class ColumnList:
                 cur.execute(f'INSERT INTO {self.table_name} ({self.column_name}) VALUES (?)', (value,))
             except:
                 pass
-            cur.execute(f'INSERT INTO {self.link_table_name} (main_id, {self.table_name.casefold() + "_id"}) '
-                        f'SELECT Main.id, {self.table_name + ".id"} FROM {self.table_name} JOIN Main '
+            cur.execute(f'INSERT INTO {self.link_table_name} (_main_id, {self.table_name.casefold() + "_id"}) '
+                        f'SELECT Main._id, {self.table_name + "._rowid_"} FROM {self.table_name} JOIN Main '
                         f'WHERE {self.column_name} = "{value}" '
-                        f'AND (SELECT Main.id FROM Main ORDER BY Main.id DES LIMIT 1)')
+                        f'AND (SELECT Main._id FROM Main ORDER BY Main._id DES LIMIT 1)')
         con.commit()
         con.close()
 
@@ -328,18 +353,17 @@ class ColumnList:
                 cur.execute(f'INSERT INTO {self.table_name} ({self.column_name}) VALUES (?)', (value,))
             except:
                 pass
-            cur.execute(f'INSERT INTO {self.link_table_name} (main_id, {self.table_name.casefold()}_id) '
-                        f'SELECT Main.id, {self.table_name}.id FROM {self.table_name} JOIN Main '
-                        f'WHERE {self.table_name}.{self.column_name} = "{value}" AND Main.id = {r_id}')
+            cur.execute(f'INSERT INTO {self.link_table_name} (_main_id, {self.table_name.casefold()}_id) '
+                        f'SELECT Main._id, {self.table_name}._rowid_ FROM {self.table_name} JOIN Main '
+                        f'WHERE {self.table_name}.{self.column_name} = "{value}" AND Main._id = {r_id}')
 
         con = sql.connect(db_name)
         cur = con.cursor()
-        r_id = cur.execute(f'SELECT id FROM Main WHERE _rowid_ = {r_id}').fetchone()[0]
         if new_value != [''] and new_value is not None:
             for v in n_value:
                 insert_new_list_value(v)
         else:
-            cur.execute(f'INSERT INTO {self.link_table_name} (main_id, {self.table_name.casefold()}_id) '
+            cur.execute(f'INSERT INTO {self.link_table_name} (_main_id, {self.table_name.casefold()}_id) '
                         f' VALUES ({r_id}, 1)')
         con.commit()
         con.close()
@@ -347,23 +371,22 @@ class ColumnList:
     def delete_row(self, db_name, old_value, o_value, r_id):
 
         def delete_from_link_table(value):
-            cur.execute(f'DELETE FROM {self.link_table_name} WHERE main_id = {r_id} AND '
-                        f'{self.table_name.casefold()}_id = (SELECT id FROM {self.table_name}'
+            cur.execute(f'DELETE FROM {self.link_table_name} WHERE _main_id = {r_id} AND '
+                        f'{self.table_name.casefold()}_id = (SELECT _rowid_ FROM {self.table_name}'
                         f' WHERE {self.column_name} = "{value}")')
 
         def delete_from_table(value):
             cur.execute(f'DELETE FROM {self.table_name} WHERE {self.column_name} = "{value}"')
 
         def count_values_uses(value):
-            return cur.execute(f'SELECT {self.link_table_name}.main_id, '
+            return cur.execute(f'SELECT {self.link_table_name}._main_id, '
                                f'{self.link_table_name}.{self.table_name.casefold()}_id FROM {self.link_table_name} '
-                               f'JOIN {self.table_name} '
-                               f'WHERE {self.link_table_name}.{self.table_name.casefold()}_id = {self.table_name}.id '
-                               f'AND {self.table_name}.{self.column_name} = "{value}"').fetchall()
+                               f'JOIN {self.table_name} WHERE {self.link_table_name}.{self.table_name.casefold()}_id = '
+                               f'{self.table_name}._rowid_ AND '
+                               f'{self.table_name}.{self.column_name} = "{value}"').fetchall()
 
         con = sql.connect(db_name)
         cur = con.cursor()
-        r_id = cur.execute(f'SELECT id FROM Main WHERE _rowid_ = {r_id}').fetchone()[0]
         if not o_value:
             old_value = old_value.split(", ")
             o_value = old_value
@@ -374,7 +397,7 @@ class ColumnList:
                 if len(temp) == 1:
                     delete_from_table(v)
         else:
-            cur.execute(f'DELETE FROM {self.link_table_name} WHERE main_id = {r_id} AND '
+            cur.execute(f'DELETE FROM {self.link_table_name} WHERE _main_id = {r_id} AND '
                         f'{self.table_name.casefold()}_id = 1')
         con.commit()
         con.close()
